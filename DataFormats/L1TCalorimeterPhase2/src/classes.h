a82223b4a36877b1a26ef93cf52bd41513433e19
Jovan Mitrevski <jmitrevs@fnal.gov>
2025-10-17 13:42:18 -0500

    First version from asavincms

--- DataFormats/L1TCalorimeterPhase2/src/classes.h
+++ DataFormats/L1TCalorimeterPhase2/src/classes.h
@@ -9,6 +9,14 @@
 /*********************/
 
 #include "DataFormats/L1TCalorimeterPhase2/interface/CaloCrystalCluster.h"
+#include "DataFormats/L1TCalorimeterPhase2/interface/CaloTower.h"
+#include "DataFormats/L1TCalorimeterPhase2/interface/CaloJet.h"
+#include "DataFormats/L1TCalorimeterPhase2/interface/DigitizedClusterCorrelator.h"
+#include "DataFormats/L1TCalorimeterPhase2/interface/DigitizedClusterCorrelatorTMI18.h"
+#include "DataFormats/L1TCalorimeterPhase2/interface/DigitizedPFClusterCorrelatorTMI18.h"
+#include "DataFormats/L1TCalorimeterPhase2/interface/DigitizedCaloToCorrelatorTMI18.h"
+#include "DataFormats/L1TCalorimeterPhase2/interface/DigitizedTowerCorrelator.h"
+#include "DataFormats/L1TCalorimeterPhase2/interface/DigitizedClusterGT.h"
 #include "DataFormats/L1TCalorimeterPhase2/interface/CaloPFCluster.h"
 #include "DataFormats/L1TCalorimeterPhase2/interface/CaloJet.h"
 #include "DataFormats/L1TCalorimeterPhase2/interface/CaloTower.h"
